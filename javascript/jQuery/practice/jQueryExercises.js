ec29c6a461b962b296991b12461e5b3274b36456
Nalipp <nateplipp@gmail.com>
2016-02-25 16:26:54 +0900

    jQuery

---
+++ javascript/jQuery/practice/jQueryExercises.js
@@ -0,0 +1,4 @@
+$('div').css("background", "purple")
+$('.highlight').css('width', '200px')
+$('#third').css('border', 'solid orange 2px')
+$('div:first-of-type').css('color', 'pink')
---
+++ javascript/jQuery/practice/jQueryMethods.js
@@ -0,0 +1,9 @@
+$('img').css('width', '30%');
+$('img').css('float', 'left');
+$('img').css('margin', '10px');
+$('img').last().attr("src", "http://www.destination360.com/europe/france/paris/images/s/paris-skyline.jpg")
+$('img').first().attr("src", "http://c3e308.medialib.glogster.com/media/be/bee50ef6b26fefd5c0534be293b0f9ea66a44d037f48ac9a4bdbf987239d49f6/starfish-polka-dot-jpg.jpg")
+
+$("input").val();
+
+// $("li").addClass("correct");
