91e21f78e26d0a093516502497b0406821667163
Nalipp <nateplipp@gmail.com>
2016-02-26 18:01:57 +0900

    todo app

--- javascript/jQuery/toDoListProject/assets/js/toDos.js
+++ javascript/jQuery/toDoListProject/assets/js/toDos.js
@@ -1,8 +1,22 @@
-$('li').click(function(){
+$('ul').on('click', 'li', function(){
   $(this).toggleClass("completed");
 });
 
-$('span').click(function(event){
-  $(this).css('display', 'none');
+$('ul').on('click', 'span', function(event){
+  $(this).parent().fadeOut(600,function(){
+    $(this).remove();
+  });
   event.stopPropagation();
+})
+
+$('input[type="text"').keypress(function(event){
+  if(event.which === 13){
+    var todoText = $(this).val();
+    $(this).val('');
+    $('ul').append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+  }
+});
+
+$('.fa-plus').on('click', function(){
+  $('input[type="text"').fadeToggle();
 })
